c6f5c7bc30d99d7e83ecdfb4492f83d957fe3409
kochetov <kochetov@alkor.co.ru>
2023-02-09 09:37:58 +0300

    added mvp structure of site

--- src/main/kotlin/Client.kt
+++ src/main/kotlin/Client.kt
@@ -1,13 +1,49 @@
 import kotlinx.browser.document
+import kotlinx.dom.createElement
 import react.create
 import react.dom.client.createRoot
 
 fun main() {
+//    document.bgColor = "#0a192f"
+    // Favicon
+    val iconLink = document.createElement("link") {
+        setAttribute("rel", "icon")
+        setAttribute("type", "image/png")
+        setAttribute("href", "./favicon.png")
+    }
+    document.head?.appendChild(iconLink)
+
+    // Mobile style
+    val mobileStyle = document.createElement("style") {
+        textContent = """
+            @media only screen and (max-width: 767px) {
+                /* Your mobile styles here */
+            }
+        """
+    }
+    document.head?.appendChild(mobileStyle)
+
+    // Fonts
+    val styleLink = document.createElement("link") {
+        setAttribute("rel", "stylesheet")
+        setAttribute("href", "https://fonts.googleapis.com/css2?family=Roboto&display=swap")
+    }
+    document.head?.appendChild(styleLink)
+
+    val style = document.createElement("style")
+    style.innerHTML = """
+        body {
+            font-family: 'Roboto', sans-serif;
+        }
+    """
+    document.head?.appendChild(style)
+
+    // Body
     val container = document.createElement("div")
     document.body?.appendChild(container)
 
     val welcome = Welcome.create {
         name = "Kotlin/JS"
     }
-    createRoot(container).render(welcome)
+    createRoot(container).render(Welcome.create())
 }
--- src/main/kotlin/Welcome.kt
+++ src/main/kotlin/Welcome.kt
@@ -1,37 +1,106 @@
-import csstype.px
-import csstype.rgb
+import csstype.*
+import emotion.react.css
 import react.FC
 import react.Props
-import emotion.react.css
-import react.dom.html.InputType
+import react.dom.html.ReactHTML.a
 import react.dom.html.ReactHTML.div
-import react.dom.html.ReactHTML.input
+import react.dom.html.ReactHTML.img
 import react.useState
 
 external interface WelcomeProps : Props {
     var name: String
 }
 
+// TODO:
+//  1. Page style
+//  2. Effects
+//  3. Flex mobile style
+//  4. Content
+
 val Welcome = FC<WelcomeProps> { props ->
     var name by useState(props.name)
     div {
         css {
-            padding = 5.px
-            backgroundColor = rgb(8, 97, 22)
-            color = rgb(56, 246, 137)
+            justifyContent = JustifyContent.center
+            alignItems = AlignItems.center
+//            display = Display.flex
+            flexDirection = FlexDirection.column
         }
-        +"Hello, $name"
-    }
-    input {
-        css {
-            marginTop = 5.px
-            marginBottom = 5.px
-            fontSize = 14.px
+        div {
+            css {
+                height = 42.px
+//            backgroundColor = Color("#ffffff")
+            }
+            div {
+                css {
+//                color = Color("#64ffda")
+                    color = Color("#000000")
+                    float = Float.left
+                }
+                img {
+                    css {
+                        width = 42.px
+                        height = 42.px
+                        float = Float.left
+                        borderRadius = 50.px
+                    }
+                    src = "https://avatars.githubusercontent.com/u/30999008"
+                }
+                +"Vladislav Kochetov"
+            }
+            div {
+//            css {
+//                position = Position.absolute
+//                top = 10.px
+//                right = 10.px
+//            }
+                a {
+                    css {
+                        color = Color("#000000")
+                    }
+                    href = "https://github.com/vladleesi"
+                    +"github.com/vladleesi"
+                }
+            }
         }
-        type = InputType.text
-        value = name
-        onChange = { event ->
-            name = event.target.value
+        div {
+            css {
+                marginTop = 20.px
+            }
+            +"I'm incredible software engineer with 5 years of experience."
+        }
+        div {
+            css {
+                marginTop = 50.px
+            }
+            +"Projects"
+        }
+        div {
+            css {
+                display = Display.flex
+//                justifyContent = JustifyContent.center
+                flexDirection = FlexDirection.row
+            }
+            div {
+                a {
+                    css {
+                        color = Color("#000000")
+                    }
+                    href = "https://github.com/vladleesi/chronicle"
+                    +"chronicle"
+                }
+            }
+            div {
+                a {
+                    css {
+                        color = Color("#000000")
+                        marginLeft = 20.px
+                    }
+                    href = "https://github.com/vladleesi/ultimate-scanner-android"
+                    +"scanner"
+                }
+            }
         }
     }
 }
+
